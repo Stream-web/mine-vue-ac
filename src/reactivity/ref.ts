2c6efd9d6058956b191a4ce0d33fcfd56f0ddc59
15398271468 <2450082265@qq.com>
2022-05-21 17:49:02 +0800

    添加ref文件和测试

---
+++ src/reactivity/ref.ts
@@ -0,0 +1,56 @@
+import { hasChanged, isObject } from './../shared/idnex';
+import { isTracking, trackEffects, triggerReffects } from "./effect";
+import { reactive } from './reactive';
+
+class RefIml{
+    private _value: any;
+    // 
+    public dep
+    private _rawValue:any;
+    public _v_isRef = true;
+    constructor(value){
+        this._rawValue = value;
+        // this._value = isObject(value) ? reactive(value):value;
+        this._value=convert(value)
+        // 如果value是对象的话那么就要做一层处理把value->reactive
+        // 1、看看value是不是对象
+
+        this.dep = new Set();
+    }
+    get value(){
+        trackRefValue(this);
+        // trackEffects(this.dep);
+        return this._value
+    }
+    set value(newValue){
+        // 一定先去修改了value 的值
+        // hasChanged
+        // 对比的时候 object
+        if(hasChanged(newValue,this._rawValue)) {
+            this._rawValue = newValue
+            // this._value =  isObject(newValue) ? reactive(newValue):newValue;
+            this._value = convert(newValue)
+            triggerReffects(this.dep);
+        }
+    }
+}
+
+function convert(value){
+    return isObject(value) ? reactive(value): value
+}
+function trackRefValue(ref){
+    if(isTracking()){
+        trackEffects(ref.dep);
+    }
+}
+export function ref(value){
+    return new RefIml(value);
+}
+export function isRef(ref){
+    return !!ref._v_isRef
+}
+export function unRef(ref){
+    // 如果是ref对象，那么直接返回出去就行了，如果不是ref
+
+    return isRef(ref) ? ref.value : ref;
+}
