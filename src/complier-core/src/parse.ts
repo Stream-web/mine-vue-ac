9ecffda1434174093e8257d6ba21f32a40d06898
15398271468 <2450082265@qq.com>
2022-06-05 16:59:40 +0800

    解析element类型

--- src/complier-core/src/parse.ts
+++ src/complier-core/src/parse.ts
@@ -1,5 +1,8 @@
 import { NodeTypes } from "./ast";
-
+const enum TagType {
+    Start,
+    End,
+}
 export function baseParse(content: string) {
     const context = createParseContext(content);
     return createRoot(
@@ -7,14 +10,45 @@ export function baseParse(content: string) {
     ) 
 }
 
-function parseChildren(content) {
+function parseChildren(context) {
     const nodes:any = [];
     let node;
-    if(content.source.startsWith("{{")){
-        node = parseInterpolation(content);
+    const s = context.source
+    if(s.startsWith("{{")){
+        node = parseInterpolation(context);
+    } else if(s[0] === "<") {
+        if(/[a-z]/i.test(s[1])) {
+            // console.log("parse element");
+            node = parseElement(context);
+        }
     }
     nodes.push(node);
     return nodes;
+}
+function parseElement(context:any){
+    const element  = parseTag(context,TagType.Start);
+ 
+ 
+    parseTag(context,TagType.End);
+
+    console.log("___________",context.source);
+
+    return element
+}
+function parseTag(context:any,type:TagType){
+    // 1 解析Tag
+    // 2 删除处理完成的代码
+    const match:any = /^<\/?([a-z]+)/i.exec(context.source)
+    console.log(match)
+    const tag = match[1];
+    advanceBy(context,match[0].length)
+    advanceBy(context,1)
+    console.log(context.source)
+    if(type === TagType.End)
+    return {
+        type:NodeTypes.ELEMENT,
+        tag
+    }
 }
 function parseInterpolation(context){
 
--- src/complier-core/tests/parse.spec.ts
+++ src/complier-core/tests/parse.spec.ts
@@ -15,3 +15,15 @@ describe('Parse',() =>{
         });
       });
 })
+describe('element',() =>{
+    describe("simple element div", () => {
+        test("simple interpolation", () => {
+          const ast = baseParse("<div></div>");
+    
+          expect(ast.children[0]).toStrictEqual({
+            type: NodeTypes.ELEMENT,
+            tag:"div",
+          });
+        });
+      });
+})
